ae89c6dd2d29ff1cb5b493ef1e5c514446dfef83
PioneerTiger <PioneerTiger@proton.me>
2024-09-24 20:11:57 +0800

    up

--- tf_api/utils.lua
+++ tf_api/utils.lua
@@ -4,7 +4,7 @@ local M = {}
 ---@param plugin_name string
 ---@return string
 function M.get_plugin_pwd(plugin_name)
-    return dart_utils.cwd() .. '/lua/plugins/' .. plugin_name
+    return dart_utils.cwd() .. '/' .. dart_utils.plugin_dir() .. '/' .. plugin_name
 end
 
 return M
