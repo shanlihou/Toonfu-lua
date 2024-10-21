e3e6f85270b6fbb9e9f4fea240e65607ecf06833
shanlihou <shanlihou@gmail.com>
2024-10-21 11:49:45 +0800

    up

--- tf_api/utils.lua
+++ tf_api/utils.lua
@@ -7,4 +7,13 @@ function M.get_plugin_pwd(plugin_name)
     return dart_utils.cwd() .. '/' .. dart_utils.plugin_dir() .. '/' .. plugin_name
 end
 
+function M.init_plugin(plugin_name)
+    local plugin_dir = dart_utils.plugin_dir()
+
+    package.path = package.path
+        .. ';'
+        .. './' .. plugin_dir .. '/' .. plugin_name .. '/?.lua;'
+        .. './' .. plugin_dir .. '/' .. plugin_name .. '/?/init.lua'
+end
+
 return M
